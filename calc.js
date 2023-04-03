1316cf65bf79eba7ea70261ea65a0dc3d17f8259
waltruist <127109343+waltruist@users.noreply.github.com>
2023-04-03 22:24:44 +0200

    Update calc.js
    some changes after using typescript for the first time on this example html.

--- calc.js
+++ calc.js
@@ -1,24 +1,39 @@
-const calcbtn        = document.getElementById('calcbtn');
-const fieldMethInQM  = document.getElementById('methinm3');
-const fieldMethInMol = document.getElementById('methinmol');
-const fieldMethInKg  = document.getElementById('methinkg');
-const fieldCarbInMol = document.getElementById('carbinmol');
-const fieldCarbInQM  = document.getElementById('carbinqm');
-const fieldCarbInKg  = document.getElementById('carbinkg')
- 
-const calcStart = function() {
-    var methQM = parseInt(fieldMethInQM.value);
-    var methMol= Math.round(((methQM*.76)/16.04)*1000);
-    var methKg = Math.round(methQM*.76);
-    var carbMol= Math.round(methMol*28);
-    var carbKg = Math.round((carbMol*44)/1000);
-    var carbQM = Math.round(carbKg/1.97);
-    
-    fieldMethInMol.value = methMol;
-    fieldMethInKg.value  = methKg;
-    fieldCarbInMol.value = carbMol;
-    fieldCarbInKg.value  = carbKg;
-    fieldCarbInQM.value  = carbQM;
+/*after using typescript for the first time i had
+to chnage a lot. Now I know how forgivable JS is
+about types. Already learnt a lot by just using
+typescript.
+*/
+
+var fieldMethInQM;
+var calcbtn;
+var fieldMethInMol;
+var fieldMethInKg;
+var fieldCarbInMol;
+var fieldCarbInQM;
+var fieldCarbInKg;
+
+calcbtn = document.getElementById('calcbtn');
+fieldMethInQM = document.getElementById('methinm3');
+fieldMethInMol = document.getElementById('methinmol');
+fieldMethInKg = document.getElementById('methinkg');
+fieldCarbInMol = document.getElementById('carbinmol');
+fieldCarbInQM = document.getElementById('carbinqm');
+fieldCarbInKg = document.getElementById('carbinkg');
+
+var methQM;
+
+var calcStart = function () {
+    methQM = parseInt(fieldMethInQM.innerHTML);
+    var methMol = Math.round(((methQM * .76) / 16.04) * 1000);
+    var methKg = Math.round(methQM * .76);
+    var carbMol = Math.round(methMol * 28);
+    var carbKg = Math.round((carbMol * 44) / 1000);
+    var carbQM = Math.round(carbKg / 1.97);
+    fieldMethInMol.innerHTML = methMol.toString();
+    fieldMethInKg.innerHTML = methKg.toString();
+    fieldCarbInMol.innerHTML = carbMol.toString();
+    fieldCarbInKg.innerHTML = carbKg.toString();
+    fieldCarbInQM.innerHTML = carbQM.toString();
 };
 
 calcbtn.addEventListener('click', calcStart);
